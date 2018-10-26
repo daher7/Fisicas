751f8089b3272c50cf8f1a951244d37e193ed0ef
daher7 <daher7@gmail.com>
2018-10-26 13:55:02 +0200

    RayCast

---
+++ Assets/_MyGameAssets/Scripts/RayCast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCast : MonoBehaviour {
+
+    public Transform origen;
+    public Transform destino;
+    public Text texto;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Disparar();
+        }
+	}
+
+    void Disparar() {
+        
+        bool hayImpacto = Physics.Raycast(origen.position, origen.forward);
+        Debug.DrawRay(origen.position, origen.forward * 100, Color.blue, 50);
+        texto.text = "" + hayImpacto;
+    }
+}
---
+++ Assets/_MyGameAssets/Scripts/Torreta.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torreta : MonoBehaviour {
+
+    [SerializeField] GameObject prefabBala;
+    [SerializeField] Transform esfera;
+    [SerializeField] Transform puntoGeneracion;
+    [SerializeField] int fuerza;
+
+    // Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject proyectil = Instantiate(prefabBala, puntoGeneracion.position, puntoGeneracion.rotation);
+            proyectil.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * fuerza);
+        }
+
+        float xR = Input.GetAxis("Vertical");
+        float yR = Input.GetAxis("Horizontal");
+        esfera.Rotate(xR * -1, yR, 0);
+	}
+}
