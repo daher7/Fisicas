3965754db53e6d9c22ddbda2c65bb2439f4c7f00
daher7 <daher7@gmail.com>
2018-10-29 11:00:30 +0100

    Follower

---
+++ Assets/_MyGameAssets/Scripts/Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    Rigidbody miRigidBody;
+
+    private void Start() {
+        miRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        Vector3 distancia = target.position - transform.position;
+        // Lo convertimos en distancia de 1 unidad
+        Vector3 distanciaNormalizada = distancia.normalized;
+
+        Vector3 deltaPostion = distanciaNormalizada * Time.deltaTime;
+        if(distancia.magnitude > 0.1f) {
+            // Para que cuando llegue a la esfera no de saltitos
+            miRigidBody.MovePosition(miRigidBody.position + deltaPostion);
+        }
+       
+    }
+
+}
