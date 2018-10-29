822e177f056314cc7b560753b13448370e238d30
daher7 <daher7@gmail.com>
2018-10-29 09:37:58 +0100

    RayCast

--- Assets/_MyGameAssets/Scripts/RayCast.cs
+++ Assets/_MyGameAssets/Scripts/RayCast.cs
@@ -15,11 +15,37 @@ public class RayCast : MonoBehaviour {
             Disparar();
         }
 	}
-
-    void Disparar() {
-        
-        bool hayImpacto = Physics.Raycast(origen.position, origen.forward);
-        Debug.DrawRay(origen.position, origen.forward * 100, Color.blue, 50);
-        texto.text = "" + hayImpacto;
+    // Dos Métodos para usar los RayCast
+    // RayCastAll
+    /*
+    public void Disparar() {
+        RaycastHit[] rhs = Physics.RaycastAll(
+            origen.position,
+            origen.forward);
+        // Ha colisionado con algo
+        if (rhs.Length != 0) {
+            // Recorremos el array para saber el nombre de cada objeto con el que ha impactado
+            for( int i = 0; i < rhs.Length; i++) {
+                print(rhs[i].transform.gameObject.name);
+            }
+        }
     }
+    */
+    
+    private void Disparar() {
+        RaycastHit rh;
+        // bool hayImpacto = Physics.Raycast(origen.position, origen.forward);
+        bool hayImpacto = Physics.Raycast(
+            origen.position,
+            origen.forward,
+            out rh);
+
+        // Debug.DrawRay(origen.position, origen.forward * 100, Color.blue, 50);
+        texto.text = "" + hayImpacto;
+        if (hayImpacto) {
+            string nombreObjetoImpactado = rh.transform.gameObject.name;
+            texto.text = nombreObjetoImpactado;
+        }     
+    }
+    
 }
